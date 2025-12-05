c94f8e402d6fc5498755b2d7a5f131460559c94c
kpj2006 <24ucs074@lnmiit.ac.in>
2025-12-06 01:00:24 +0530

    fix: - Moved key prop from inside Card component to parent map functions in 3 pages. - Changed overflow-hidden to overflow-visible to allow images to expand on hover and also add z-10.

--- components/card.tsx
+++ components/card.tsx
@@ -13,17 +13,16 @@ const Card: React.FC<Props> = ({ index, image, title, description, link }) => {
   return (
     <Link href={link}>
       <div
-        key={index}
-        className="group relative flex h-auto flex-col justify-evenly rounded-lg border border-zinc-200 p-6 shadow-xl"
+        className="group relative flex h-auto flex-col justify-evenly overflow-visible rounded-lg border border-zinc-200 p-6 shadow-xl"
       >
-        <div className="relative h-fit overflow-hidden rounded-md">
+        <div className="relative h-fit overflow-visible rounded-md">
           <Image
             src={image}
             alt="Protocol Logo"
             loading="lazy"
             width="700"
             height="700"
-            className="h-64 w-full object-contain transition duration-500 group-hover:scale-105"
+            className="h-64 w-full object-contain transition duration-500 group-hover:scale-105 group-hover:z-10"
           />
         </div>
         <div className="relative mt-4 h-fit">
