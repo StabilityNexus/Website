f17e2910d6f41cefe3e6ef228949cee1523800a2
piyushbijarania <piyush.gag6513@gmail.com>
2026-06-27 13:40:03 +0530

    add development section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Hero from "@/components/hero"
 import ResearchHighlights from "@/components/research-highlights"
+import Development from "@/components/development"
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <ResearchHighlights />
+      <Development />
     </main>
   )
 }
+
 
